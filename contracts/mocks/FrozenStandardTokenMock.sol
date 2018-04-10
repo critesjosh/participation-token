7a9b3543d4ebcb2ed788a00a3274af5b97b93a65
PhABC <ph.castonguay@gmail.com>
2018-04-09 20:13:07 -0400

    Set token details

--- contracts/mocks/FrozenStandardTokenMock.sol
+++ contracts/mocks/FrozenStandardTokenMock.sol
@@ -5,7 +5,12 @@ import "../token/FrozenStandardToken.sol";
 // mock class using StandardToken
 contract FrozenStandardTokenMock is FrozenStandardToken {
 
-  function FrozenStandardTokenMock(address initialAccount, uint256 initialBalance) public {
+  function FrozenStandardTokenMock(
+            address initialAccount, 
+            uint256 initialBalance) 
+            public 
+            DetailedERC20("NAME", "SYM", 18) 
+  {
     balances[initialAccount] = initialBalance;
     totalSupply_ = initialBalance;
   }
