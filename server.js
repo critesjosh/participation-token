8cf929a8fe3e2554ab43fbdb9c588e210fba3e3f
Josh <critesjosh@gmail.com>
2018-04-12 14:01:03 +0000

    added ssl path for server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http')
+var fs = require('fs')
 //var https =require("https")
 var path = require('path')
 var bodyParser = require('body-parser')
@@ -9,14 +10,19 @@ var api = require("./api/api")
 var authenticate = require('./utils/authenticate')
 var ptcInstance = require('./utils/getContracts')
 
-var express = require('express');
+var express = require('express')
 var app = express();
 
+const credentials = {
+  cert: fs.readFileSync("/etc/letsencrypt/live/aqui.joshcrites.com/fullchain.pem"),
+  key: fs.readFileSync("/etc/letsencrypt/live/aqui.joshcrites.com/privkey.pem")
+}
+//var server = https.createServer(credentials, app)
 var server = http.createServer(app);
 
 var mongoose = require('mongoose');
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/participation-token";
+var url = "mongodb://localhost:27017/participation-token"
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -37,5 +43,5 @@ app.get('/*', function(req, res) {
 
 server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+  console.log("Chat server listening at", addr.address + ":" + addr.port)
 });
